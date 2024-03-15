a35a92da6f334ebdc821c8a9f6626993113b239d
Kelly Schumacher <kschumacher93@gmail.com>
2024-03-15 18:08:40 -0400

    js for button clcik

---
+++ ToastNotif/toastnotif.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+
+const messages = [
+  "Message One",
+  "Message Two",
+  "Message Three",
+  "Message Four",
+];
+
+button.addEventListener("click", () => createNotification());
+function createNotifcation() {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.innerText = getRandomMessage();
+  toasts.appendChild(notif);
+}
