4ad720dbc2928f61585d1eba8f051ed4f90866e9
Kelly Schumacher <kschumacher93@gmail.com>
2024-03-16 12:32:02 -0400

    added randomized color set timeout to disapear

--- ToastNotif/toastnotif.js
+++ ToastNotif/toastnotif.js
@@ -1,5 +1,5 @@
-const button = document.getElementById("button");
 const toasts = document.getElementById("toasts");
+const button = document.getElementById("button");
 
 const messages = [
   "Message One",
@@ -7,11 +7,25 @@ const messages = [
   "Message Three",
   "Message Four",
 ];
-
+const types = ["info", "success", "error"];
 button.addEventListener("click", () => createNotification());
-function createNotifcation() {
+
+function createNotification(message = null, type = null) {
   const notif = document.createElement("div");
   notif.classList.add("toast");
-  notif.innerText = getRandomMessage();
+  notif.classList.add(type ? type : getRandomType());
+
+  notif.innerText = message ? message : getRandomMessage();
+
   toasts.appendChild(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 3000);
+}
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
+}
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)];
 }
